244ab7d9b936c17866531af9a562ca9e9d9bc702
Gabe <gabe@paperlesspost.com>
2014-07-20 02:42:17 -0400

    cookie-based authentication

--- app/rotten.rb
+++ app/rotten.rb
@@ -1,9 +1,26 @@
-require 'sinatra'
+require 'bundler'
+Bundler.require
+
+load '.env' if File.exists?('.env')
 
 class RottenApp < Sinatra::Base
-  get '/' do
-    "Hello World!"
+  configure do
+    redistogo = URI.parse ENV['REDISTOGO_URL']
+    set :redis => Redis.new(:host => redistogo.host,
+                            :port => redistogo.port,
+                            :password => redistogo.password)
+  end
+  $redis = settings.redis
+
+  get '/cbt.rss' do
+    begin
+      CBT.sources.each do # ...
+      end
+    rescue => e
+    end
   end
 
   run! if app_file == $0
 end
+
+require_relative 'sites/cbt.rb'
---
+++ app/sites/cbt.rb
@@ -0,0 +1,29 @@
+class CBT
+  include HTTParty
+  base_uri $redis.hget(:cbt, :base_uri)
+
+  def self.sources
+    response = get('/browse.php', headers: {
+      "Cookie" => $redis.hget(:cbt, :cookie)}
+    )
+    raise "Session Expired" if response.match('takelogin')
+    require 'pry'; binding.pry
+  end
+
+private
+
+  def self.reset_cookie
+    response = get('/login.php')
+    raise "CAPTCHA Required" if response.match('captcha')
+
+    print 'Username: '; username = gets.strip
+    print 'Password: '; password = gets.strip
+    response = post('/takelogin.php', body: {
+      username: username, password: password
+    })
+    cookie = response.request.options[:headers]['Cookie']
+    $redis.hset :cbt, :cookie, cookie
+    cookie
+  end
+
+end
