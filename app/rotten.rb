4c0a701b0157efe85a769cdde53c5aa03d3a561e
Gabe <gabe@paperlesspost.com>
2014-07-21 11:11:10 -0400

    parse sources from rows

--- app/rotten.rb
+++ app/rotten.rb
@@ -7,17 +7,16 @@ $redis = Redis.new(:host => redistogo.host,
                    :port => redistogo.port,
                    :password => redistogo.password)
 
+require_relative 'sites/cbt.rb'
+
 class RottenApp < Sinatra::Base
 
   get '/cbt.rss' do
     begin
-      CBT.sources.each do # ...
-      end
+      CBT.sources.map(&:to_rss) #.join(...)
     rescue => e
     end
   end
 
   run! if app_file == $0
 end
-
-require_relative 'sites/cbt.rb'
--- app/sites/cbt.rb
+++ app/sites/cbt.rb
@@ -2,17 +2,28 @@ class CBT
   include HTTParty
   base_uri $redis.hget(:cbt, :base_uri)
 
+  ROW_REGEX = /<tr class="(?:even|odd)">.*?<\/tr>/m
   def self.sources
     response = get('/browse.php', headers: {
       "Cookie" => $redis.hget(:cbt, :cookie)}
     )
     raise "Session Expired" if response.match('takelogin.php')
-    require 'pry'; binding.pry
+    response.scan(ROW_REGEX).map{|row| Source.from_row(row)}
+  end
+
+  class Source
+    attr_accessor :row
+    def self.from_row(row)
+      self.row = row
+    end
+
+    def to_rss
+    end
   end
 
 private
 
-  def self.reset_cookie
+  def self.reset_cookie # To be manually run from pry
     response = get('/login.php')
     raise "CAPTCHA Required" if response.match('captcha')
 
@@ -22,7 +33,7 @@ private
       username: username, password: password
     })
     cookie = response.request.options[:headers]['Cookie']
-    $redis.hset :cbt, :cookie, cookie
+    $redis.hset(:cbt, :cookie, cookie)
     cookie
   end
 
