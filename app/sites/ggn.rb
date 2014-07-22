313054b43e2a9f94d5c6b3215b0087977d6e8178
Gabe <gabe@paperlesspost.com>
2014-07-22 12:59:25 -0400

    fix GGn cookie fetching, and a working browse

--- app/sites/ggn.rb
+++ app/sites/ggn.rb
@@ -3,9 +3,10 @@ class GGn
   base_uri $redis.hget(:ggn, :base_uri)
 
   def self.items
-    # response = get('/browse.php', headers: {
-    #   "Cookie" => $redis.hget(:ggn, :cookie)
-    # })
+    response = get('/browse.php', verify: false,
+      headers: {'cookie' => $redis.hget(:ggn, :cookie)}
+    )
+
     # raise "Session Expired" if response.match('login.php')
     # response.scan(ROW_REGEX).map{|row| Item.from_row(row)}
   end
@@ -15,21 +16,19 @@ private
   def self.reset_cookie # To be manually run from pry
     response = get('/login.php', verify: false)
     #raise "CAPTCHA Required" if response.match('captcha')
-
     print 'Username: '; username = gets.strip
     print 'Password: '; password = gets.strip
 
     # Figure out how to POST to HTTPS with HTTParty...
     uri = URI.parse(base_uri+'/login.php')
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Post.new(uri.request_uri)
-    request.set_form_data(
-      username: username, password: password
-    )
-    response = http.request(request)
-    cookie = response['set-cookie']
+    options = {use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE}
+    response = Net::HTTP.start(uri.host, uri.port, options) do |http|
+      http.request(Net::HTTP::Post.new(uri.request_uri).tap{|request|
+        request.set_form_data(username: username, password: password)
+      })
+    end
+    cookie = response['set-cookie'].split(/;|,/).select{|part|
+             part.match(/__cfduid|PHPSESSID|session/)}.join(';')
     $redis.hset(:ggn, :cookie, cookie)
     cookie
   end
