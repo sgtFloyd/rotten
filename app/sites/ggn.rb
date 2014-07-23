62e8f65d806a34d0b315559d592fbfbb3350b962
Gabe <gabe@paperlesspost.com>
2014-07-23 13:28:27 -0400

    parse GGn rows

--- app/sites/ggn.rb
+++ app/sites/ggn.rb
@@ -2,15 +2,36 @@ class GGn
   include HTTParty
   base_uri $redis.hget(:ggn, :base_uri)
 
-  ROW_REGEX = /<tr class="group_.*?">\s*<td colspan="3">.*?<\/tr>/m
+  COL_REGEX = /<td.*?<\/td>/m
 
   def self.items
     response = get($redis.hget(:ggn, :browse_path), verify: false,
       headers: {'cookie' => $redis.hget(:ggn, :cookie)}
     )
-    response.scan(ROW_REGEX).map{|row| Item.from_row(row)}
+    response.scan(ROW_REGEX).map{|row| Item.from_row(row)} +
+      response.scan(ALT_ROW_REGEX).map{|row| Item.from_alt_row(row)}
   rescue HTTParty::RedirectionTooDeep => e
     raise e.response.location.match('login.php') ? 'Session Expired' : e
+  end
+
+  class Item < OpenStruct
+    CONTENT = />(.*?)</m
+
+    def self.from_row(row)
+      cols = row.scan(COL_REGEX)
+      self.new(
+        title: cols[2].scan(CONTENT).join.gsub(/\s+/, ' ').strip,
+        link:  [GGn.base_uri,'/',cols[2].scan(/<a.*?href="(.*?)".*?>/m)[0]].join,
+        size:  cols[6].scan(CONTENT).join,
+        total: cols[7].scan(CONTENT).join.to_i,
+        up:    cols[8].scan(CONTENT).join.to_i,
+        down:  cols[9].scan(CONTENT).join.to_i,
+        date:  Time.parse(cols[4].scan(/title="(.*?)"/).join),
+      )
+    end
+
+    def self.from_alt_row(alt_row)
+    end
   end
 
 private
